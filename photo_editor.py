8432e59b0660c6a981dd267c1cb44d12029f6051
sofa <sofiya.chumakova@gmail.com>
2021-12-08 23:17:58 +0300

    #1 Каркас программы, меню, открытие изображений [ч.1]

--- photo_editor.py
+++ photo_editor.py
@@ -1,5 +1,7 @@
 from tkinter import *
-
+import tkinter.filedialog as fd
+from PIL import Image, ImageTk
+import os
 
 class PyPhotoEditor:
     def __init__(self): # конструктор класса
@@ -22,6 +24,7 @@ class PyPhotoEditor:
         menu_bar = Menu(self.root) # каркас меню
 
         file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Open", command=self.open_new_image)
         menu_bar.add_cascade(label="File", menu=file_menu)
 
         self.root.configure(menu=menu_bar)
@@ -29,13 +32,20 @@ class PyPhotoEditor:
     def draw_widgets(self):
         pass
 
+    def open_new_image(self):
+        image_path = fd.askopenfilename(filetypes=(('png files', '*.png'),('jpeg files', '*.jpeg'),('jpg files', '*.jpg'), )) #запрос картинки
+
+        image = ImageTk.PhotoImage(Image.open(image_path)) #сначала получаем путь до картинки, потом открываем картинку в формате библиотеки PIL, потом преобразуем в картинку, которую можно редактировать в Tkinter
+        image_panel=Label(self.root, image=image)
+        image_panel.image=image #сохраним картинку в панели, чтобы она сразу не закрывалась
+        image_panel.pack()
+
     def _close(self, event): # метод отвечает за закрытие приложения
         self.root.quit()
 
 
 if __name__=="__main__": #проверяет: является файл запускаемым или импортируемым(если запускаемый, то выполняется условие -->
     PyPhotoEditor().run()
-
 
 
 
